8665bf26d0c06b2c7f5b1627c1f24df5b887927f
Ivan <ronya.sveta@gmail.com>
2026-08-15 13:14:44 +0300

    Add project files.

---
+++ С#_Victoryna/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace С__Victoryna
+{
+    public class User
+    {
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        
+        public List<UserResult> Results { get; private set; } = new(); // Історія проходження вікторин
+
+        public User(string login, string password, DateTime birthDate)
+        {
+            
+            Login = login;
+            Password = password;
+            BirthDate = birthDate;
+        }
+        public void ChangePassword(string newPassword)
+        {
+            Password = newPassword;
+        }
+        public void ChangeBirthDate(DateTime newDate)
+        {
+            BirthDate = newDate;
+        }        
+        public void AddResult(UserResult result) // Додавання результату вікторини
+        {
+            Results.Add(result);
+        }
+        public override string ToString()
+        {
+            return $"{Login}|{Password}|{BirthDate:yyyy-MM-dd}";
+        }
+    }
+}
+
---
+++ С#_Victoryna/UserResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace С__Victoryna
+{
+    public class UserResult
+    {
+        public string VictorynaName { get; set; }      // Назва вікторини
+        public int CorrectAnswers { get; set; }            // Кількість правильних відповідей
+        public DateTime Date { get; set; }        // Дата проходження
+
+        public UserResult(string victorynaName, int correctAnswers, DateTime date)
+        {
+            VictorynaName = victorynaName;
+            CorrectAnswers = correctAnswers;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $" {VictorynaName}|{CorrectAnswers}|{Date:yyyy-MM-dd}";
+        }
+    }
+}
