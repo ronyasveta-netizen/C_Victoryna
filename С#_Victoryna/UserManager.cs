7084fd1a8fdc1827a92eb1808dd0c0aa3071f033
Ivan <ronya.sveta@gmail.com>
2026-08-15 23:17:35 +0300

    UserManager

--- С#_Victoryna/UserManager.cs
+++ С#_Victoryna/UserManager.cs
@@ -1,10 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace С__Victoryna
 {
     class UserManager
     {
+        private List<User> _users = new();
+
+        // Завантаження користувачів з файлу
+        public void LoadUsers()
+        {
+            if (!File.Exists("users.txt"))
+                return;
+
+            var lines = File.ReadAllLines("users.txt");
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split('|');
+                if (parts.Length != 3) continue;
+
+                string login = parts[0];
+                string password = parts[1];
+                DateTime birthDate = DateTime.Parse(parts[2]);
+
+                _users.Add(new User(login, password, birthDate));
+            }
+        }
+
+        // Збереження користувачів у файл
+        public void SaveUsers()
+        {
+            List<string> lines = new();
+
+            foreach (var u in _users)
+                lines.Add(u.ToString());
+
+            File.WriteAllLines("users.txt", lines);
+        }
+
+        // Перевірка чи логін зайнятий
+        public bool IsLoginTaken(string login)
+        {
+            return _users.Any(u => u.Login == login);
+        }
+
+        // Валідація пароля
+        private bool IsPasswordValid(string password)
+        {
+            if (password.Length < 6)
+            {
+                Console.WriteLine("Пароль має містити мінімум 6 символів.");
+                return false;
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                Console.WriteLine("Пароль має містити хоча б одну цифру.");
+                return false;
+            }
+
+            if (!password.Any(char.IsUpper))
+            {
+                Console.WriteLine("Пароль має містити хоча б одну велику літеру.");
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, @"[\W_]"))
+            {
+                Console.WriteLine("Пароль має містити хоча б один спеціальний символ (*, !, ?, @, #, %).");
+                return false;
+            }
+
+            return true;
+        }
+
+        // Реєстрація
+        public bool Register(string login, string password, DateTime birthDate)
+        {
+            if (IsLoginTaken(login))
+            {
+                Console.WriteLine("Такий логін вже існує.");
+                return false;
+            }
+
+            if (!IsPasswordValid(password))
+            {
+                Console.WriteLine("Пароль не відповідає вимогам.");
+                return false;
+            }
+
+            var user = new User(login, password, birthDate);
+            _users.Add(user);
+            SaveUsers();
+
+            Console.WriteLine("Реєстрація успішна!");
+            return true;
+        }
+
+        // Логін
+        public User Login(string login, string password)
+        {
+            foreach (var u in _users)
+                if (u.Login == login && u.Password == password)
+                    return u;
+
+            return null;
+        }
+
+        // Зміна пароля
+        public bool ChangePassword(User user, string newPassword)
+        {
+            if (!IsPasswordValid(newPassword))
+                return false;
+
+            user.ChangePassword(newPassword);
+            SaveUsers();
+            return true;
+        }
+
+        // Зміна дати народження
+        public bool ChangeBirthDate(User user, DateTime newDate)
+        {
+            user.ChangeBirthDate(newDate);
+            SaveUsers();
+            return true;
+        }
     }
 }
---
+++ С#_Victoryna/VictorynaManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace С__Victoryna
+{
+    class VictorynaManager
+    {
+
+    }
+}
